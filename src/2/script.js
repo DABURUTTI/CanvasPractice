563fc58c6cadb40641edfec6cff9fa34b8db561e
DABU <daburutti@gmail.com>
2020-10-12 16:33:02 +0900

    とてもかしこいID振り分け

--- src/2/script.js
+++ src/2/script.js
@@ -1,11 +1,16 @@
 var dots = new Array();
+let myFont;
+function preload() {
+  myFont = loadFont('../../Assets/NotoSansJP-Black.otf');
+}
 
 function setup() {
-    createCanvas(document.body.clientWidth,document.documentElement.clientHeight, WEBGL);
+  createCanvas(document.body.clientWidth,document.documentElement.clientHeight, WEBGL);
+  textFont(myFont);
     var num = 0;
     for(var i = 0; i < 10; i ++){
       for(var j = 0; j < 10; j++){
-        var b  = new Point(i * 100,j * 100,0,50,num);
+        var b  = new Point(i ,j ,0,50,num);
         num = num + 1;
         dots.push(b);
       }
@@ -29,30 +34,98 @@ function draw() {
 class Point {
   
     constructor(x,y,z,size,id) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
+      this.x_o = x* 100;
+      this.y_o = y* 100;
+      this.z_o = z* 100;
+      this.x = x* 100;
+      this.y = y* 100;
+      this.z = z* 100;
       this.size = size;
-      this.id = id;
-    }
-  
-    update(x,y,z,size) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
-      this.size = size;
+      this.id = this.calcID(x,y);
     }
 
-    updateByown(){
-      this.z = sin(radians(frameCount*1 + this.x *0.1 + this.y*0.1))*50;
-      return [0,0,z];
+    calcID(x,y){
+      var px = 1;
+      var py = 1;
+
+      // for(var i = x; x > 0 ; i--){
+      //   for(var j = y ; y > 0 ; j--){
+
+      //   }
+      // }
+
+      x ++;
+      y ++;
+
+      var num = 0;
+      while(true){
+        num = num + 1;
+        if(px === x){
+          if(py == y){
+            break;
+          }
+        }
+
+        if(px === 10){
+          px = py+1;
+          py = 10;
+          continue;
+        }
+
+        if(py === 1){
+          py = px + 1;
+          px = 1;
+          continue;
+        }
+        
+        px ++;
+        py --;
+        console.log(px);
+      }
+      return num;
+      // var num = 1;
+      // for(var j = 0; j < y+1 ; j++){
+      //   num = num + j;
+      // }
+
+      // for(var j = 0; j < x ; j++){
+      //   num = num + y + j + 2;
+      // }
+      // return num;
     }
 
-    update(){
-      var x = sin(radians(map(this.id, 0, 100, 0, 360)) + frameCount)*1000;
-      var y = cos(radians(map(this.id, 0, 100, 0, 360)) + frameCount)*1000;
+    // update(x,y,z,size) {
+    //   this.x = x;
+    //   this.y = y;
+    //   this.z = z;
+    //   this.size = size;
+    // }
+
+    update() {
+    
+      var  value = frameCount % 700;
+      //var  value = 0;
+       var vector = [this.move_rows(),this.move_circle()];
+       this.x = map(value, 0, 700, vector[0][0], vector[1][0]);
+       this.y = map(value, 0, 700, vector[0][1], vector[1][1]);
+       this.z = map(value, 0, 700, vector[0][2], vector[1][2]);
+    }
+
+    move_rows(){
+      var x = this.x_o;
+      var y = this.y_o;
+      var z = sin(radians(frameCount*1 + this.x *0.1 + this.y*0.1))*50;
+      return [x,y,z];
+    }
+
+    move_circle(){
+      var x = sin(radians(map(this.id, 0, 100, 0, 360)) + 0)*1000;
+      var y = cos(radians(map(this.id, 0, 100, 0, 360)) + 0)*1000;
       return [x,y,0];
     }
+
+
+
 
     draw() {
       noStroke();
@@ -64,6 +137,8 @@ class Point {
       //rotateY(frameCount * 0.1);
       //rotateZ(radians(90));
       ellipse(0,0,this.size,this.size);
+      fill(0);
+      text(this.id,-3,3);
       pop();
     }
   }
